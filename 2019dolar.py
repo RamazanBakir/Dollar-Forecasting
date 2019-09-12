f2648aaa62bd172976239435bd1ef8bdbfa90282
RamazanBakir <48602486+RamazanBakir@users.noreply.github.com>
2019-09-12 14:08:31 +0300

    Update 2019 Dolar Forecasting
    You can also try it for another year.I am adding the used data in csv format.

--- 2019dolar.py
+++ 2019dolar.py
@@ -11,8 +11,9 @@ veri = pd.read_csv("2019dolaralis.csv")
 x = veri["Gun"]
 y = veri["Fiyat"]
 
-x=x.ravel().reshape(97,1)
-y=y.ravel().reshape(97,1)
+
+x=x.ravel().reshape(len(veri),1)
+y=y.ravel().reshape(len(veri),1)
 
 plt.title("Lineer ve POlinom REgresyon")
 plt.xlabel("Gun")
@@ -21,8 +22,8 @@ plt.grid()
 plt.scatter(x,y)
 #plt.show()
 
-#Lineer REgresyon
-tahminlineer = LinearRegression()
+#Lineer Regresyon
+tahminlineer=LinearRegression()
 tahminlineer.fit(x,y) #x ve y eksenine oturtmak için.
 tahminlineer.predict(x) #günlere göre fiyatları arıyoruz.Predict tahmin etmesi için.
 plt.plot(x,tahminlineer.predict(x),c="red") #çizdirmek için
@@ -30,39 +31,20 @@ plt.plot(x,tahminlineer.predict(x),c="red") #çizdirmek için
 #plt.show()
 
 #Polinom Regresyon
-#2.dereceden Regresyon için
-tahminpolinom = PolynomialFeatures(degree=2)#kaçıncı dereceden polinom olacağını tanımladık.(3.dereceden)
-Xyeni = tahminpolinom.fit_transform(x)#x in yeni matrisini oluşturuyor.
+a=[2,3,9]
+b=["black","green","gray"]
+i=0
+for i in range(len(a)):
+    tahminpolinom = PolynomialFeatures(degree=a[i])#kaçıncı dereceden polinom olacağını tanımladık.
+    Xyeni = tahminpolinom.fit_transform(x)#x in yeni matrisini oluşturuyor.
 
-polinommodel = LinearRegression() 
-polinommodel.fit(Xyeni,y) #Yeni matrisle y değerini eksene oturttuk.
-polinommodel.predict(Xyeni) #Yeni matrise göre tahmin ettiriyoruz.
-plt.plot(x,polinommodel.predict(Xyeni),c="black") #
-#plt.show()
-#3.dereceden Regresyon için
-tahminpolinom3=PolynomialFeatures(degree=3)
-Xyeni=tahminpolinom3.fit_transform(x)
-
-polinommodel3=LinearRegression()
-polinommodel3.fit(Xyeni,y)
-polinommodel3.predict(Xyeni)
-plt.plot(x,polinommodel3.predict(Xyeni),c="green")
-#plt.show()
-#9.dereceden Regresyon için
-tahminpolinom9=PolynomialFeatures(degree=9)
-Xyeni=tahminpolinom9.fit_transform(x)
-
-polinommodel9=LinearRegression()
-polinommodel9.fit(Xyeni,y)
-polinommodel9.predict(Xyeni)
-plt.plot(x,polinommodel9.predict(Xyeni),c="gray")
+    polinommodel = LinearRegression() 
+    polinommodel.fit(Xyeni,y) #Yeni matrisle y değerini eksene oturttuk.
+    polinommodel.predict(Xyeni) #Yeni matrise göre tahmin ettiriyoruz.
+    plt.plot(x,polinommodel.predict(Xyeni),c=b[i]) #
+    i=i+1
 plt.show()
-"""
-#Hataları görmek için 
-verilerimizin tahmin ettiğimiz değerle gerçek değer arasında ki verinin 
-karesini alıp toplayıp,tüm değerler için yani bunu yapıp toplayıp,
-Hangi modelde daha az hata var yada çok hata var GÖrebiliriz.
-"""
+#Hangi derecenin daha iyi olduğunu bulmak için.
 a=0
 hatakaresipolinom=0
 for a in range(50):
@@ -82,15 +64,16 @@ for a in range(50):
 
     hatakaresipolinom = 0
 
+"""
+#Hataları görmek için 
+verilerimizin tahmin ettiğimiz değerle gerçek değer arasında ki verinin 
+karesini alıp toplayıp,tüm değerler için yani bunu yapıp toplayıp,
+Hangi modelde daha az hata var yada çok hata var GÖrebiliriz.
+"""
 hatakaresilineer=0
 hatakaresipolinom=0
-
 #Polinom regresyonun hatasını görmek için....
-for i in range(len(Xyeni)):              #(Gerçek değeriöm - tahmini değerim)**2
+for i in range(len(Xyeni)):              #(Gerçek değerim - tahmini değerim)**2
     hatakaresipolinom=hatakaresipolinom+(float(y[i]-float(polinommodel.predict(Xyeni)[i])))**2  
 for i in range(len(y)):
     hatakaresilineer = hatakaresilineer + (float(y[i])-float(tahminlineer.predict(x)[i]))**2
-
-#Hangi derecenin daha uygun olduğunu bulmak için 
-hatakaresipolinom = 0
-    
